57c7a089c0e1e5c0a9cfefc53d9e0abe97fba8ee
Sharik-pixel <anaraturdumanbetova@gmail.com>
2020-11-03 17:22:13 -0500

    no treat or trick commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,5 +1,11 @@
 public class HalloweenTest {
 
     // I am about to give everyone candy fo Halloween
+    // are we there yet???
+    // lets play a game
+    // its fun to drive so far
+
+    // there will be no treat and trick this year
+    // stay home and be safe and wear your mask
 
 }
